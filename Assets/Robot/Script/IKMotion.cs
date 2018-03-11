cb706b77e6b156ae05107662069bb5be6a842f97
GVatinno <gianluca.vatinno@gmail.com>
2018-03-11 17:15:52 +0000

    better look at

--- Assets/Robot/Script/IKMotion.cs
+++ Assets/Robot/Script/IKMotion.cs
@@ -6,23 +6,27 @@ public class IKMotion
 {
 	private Animator mAnimator = null;
 	private Vector3 mTarget;
+	private Vector3 mPrevTarget;
 	private float mWeight = 0.0f;
 
 	public IKMotion(Animator animator)
 	{
 		mAnimator = animator;
+		mPrevTarget = Vector3.zero;
+		mTarget = Vector3.zero;
 	}
 	
 	public void SetTarget( Vector3 target )
 	{
+		mPrevTarget = mTarget;
 		mTarget = target;
 		mWeight = 0.0f;
 	}
 
 	public void IKPass () 
 	{
-		mAnimator.SetLookAtPosition (mTarget);
-		mAnimator.SetLookAtWeight (mWeight);
+		mAnimator.SetLookAtPosition(Vector3.LerpUnclamped(mPrevTarget, mTarget, mWeight));
+		mAnimator.SetLookAtWeight (1.0f);
 		mWeight += 0.01f;
 	}
 }
